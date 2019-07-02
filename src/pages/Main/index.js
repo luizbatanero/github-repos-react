6b31758be42ddb1a8e3cc3cafca6fcbee7a46f30
Luiz Batanero <luizbatanero@gmail.com>
2019-07-02 01:13:32 -0300

    Save state to localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,22 @@ class Main extends Component {
     loading: false,
   };
 
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleInputChange = e => {
     this.setState({ newRepo: e.target.value });
   };
