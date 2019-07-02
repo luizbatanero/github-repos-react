9472ff19a3cc8910cb5f51ecd60b591fa20c8ab3
Luiz Batanero <luizbatanero@gmail.com>
2019-07-02 00:41:41 -0300

    Main styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 const Main = () => {
-  return <h1>Main</h1>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 };
 
 export default Main;
