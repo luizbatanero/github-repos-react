a7c335e3771036f031a035d5c4a511c352089206
Luiz Batanero <luizbatanero@gmail.com>
2019-07-02 01:35:51 -0300

    Show repository info

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 
 import api from '../../services/api';
 
-import { Container, Form, SubmitButton, List } from './styles';
+import Container from '../../components/Container';
+import { Form, SubmitButton, List } from './styles';
 
 class Main extends Component {
   state = {
